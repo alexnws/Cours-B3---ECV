c26def215ca2f7894da333ec66dda0c573c12881
Alex <alex@macbook-air-de-alex.home>
2023-03-12 16:41:14 +0100

    Creation de la fonctionnalité delete

--- code/public/rendu/delete.php
+++ code/public/rendu/delete.php
@@ -1,5 +1,21 @@
-<h1>delete</h1>
+<?php
 
+require_once 'fonction.ini.php';
+$connexion = connexion();
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $id = strip_tags($_GET['id']);
+    $sql = "DELETE FROM `Films` WHERE `id`=:id;";
+
+    $query = $connexion->prepare($sql);
+
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+
+    header('Location: read.php');
+}
+
+require_once('close.php');
 //todo: Recuperer l'id de l'url 
 //todo: Faire la requete SQL correspondante
 //todo: Bonus: mettre une confirmation de suppression avant d'exécuter la requete
